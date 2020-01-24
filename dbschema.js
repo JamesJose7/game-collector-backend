1ecd7323cbfb910bd5012fcbde01218b2b4ecb2b
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2020-01-23 22:57:38 -0500

    Added DB schema

---
+++ dbschema.js
@@ -0,0 +1,39 @@
+let db = {
+    users: [
+        {
+            userId: 'sfoisjdfoj2or23o',
+            email: 'email@host.com',
+            username: 'user',
+            createdAt: 'date'
+        }
+    ],
+    games: [
+        {
+            user: 'username',
+            dateAdded: 'date',
+            imageUri: 'http://url.png',
+            isPhysical: true | false,
+            name: 'The Legend of Zelda: Breath of the wild',
+            shortName: 'TLoZ: Breath of the Wild',
+            platformId: 'platformId',
+            platform: 'Nintendo Switch',
+            publisherId: 'publisherId',
+            publisher: 'Nintendo',
+            timesCompleted: 1
+        }
+    ],
+    platforms: [
+        {
+            platformId: 'odsfnsoidnfos',
+            name: 'Nintendo Switch',
+            imageUri: 'http://url.png',
+            color: '#ff000a'
+        }
+    ],
+    publishers: [
+        {
+            publisherId: 'sdofijsoifjs',
+            name: 'Nintendo'
+        }
+    ]
+};
